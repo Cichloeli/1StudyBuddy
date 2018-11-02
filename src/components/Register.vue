d72bff6009f3f5940bc0520b5ce4c9911d938b2c
Saiya Youn <sayoun@ucsc.edu>
2018-11-02 16:41:16 -0700

    Created Register.vue

---
+++ src/components/Register.vue
@@ -0,0 +1,9 @@
+<template>
+ <div class ="register">
+     <p>Let's create a new account!</p>
+     <input type="text" placeholder="Email">
+     <input type="password" placeholder="Password">
+     <button>Register</button>
+     <span>or go back to <router-link to="/login">login</router-link>.</span>
+ </div>    
+</template>
