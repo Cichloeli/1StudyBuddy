8629bec9183dfa2be9c99a64ad05526db5c633ed
xgao21 Xuya Gao <xgao21@ucsc.edu>
2018-11-04 03:08:08 +0800

    update welcome page version 1

--- src/components/home.vue
+++ src/components/home.vue
@@ -5,13 +5,17 @@
       <div class="col-md-4 col-lg4" v-for="(data,index) in products" :key="index">
         <img :src="data.image" class="img-fluid">
          <h3 @click="goTodetail(data.productId)" >{{data.productTitle}}</h3>
-         
       </div>
     </div>
     <p>
       1StudyBuddy allows classmates to meet and form study groups. 
     </p>
+    <div class="showing detail" v-for="(data,index) in details" :key="index">
+      <p><b>{{data.detailTitle}}</b></p>
+      <p><b>{{data.detailStuff}}</b></p>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -22,7 +26,7 @@ export default {
       title: '1StudyBuddy',
       products:[
       {
-        productTitle:"Engage",
+        productTitle:"Connect",
         image       : require('../assets/images/product1.jpg'),
         productId:1
       },
@@ -32,7 +36,7 @@ export default {
         productId:2
       },
       {
-        productTitle:"Succeed",
+        productTitle:"Excel",
         image       : require('../assets/images/product3.jpg'),
         productId:3
       }/* ,
@@ -51,6 +55,20 @@ export default {
         image       : require('../assets/images/product6.png'),
         productId:6
       } */
+      ],
+      details:[
+        {
+          detailTitle: "connect",
+          detailStuff: "default connect"
+        },
+        {
+          detailTitle: "learn",
+          detailStuff: "default learn"
+        },
+        {
+          detailTitle: "excel",
+          detailStuff: "default excel"
+        }
       ]
     }
   },
@@ -73,4 +91,15 @@ export default {
 .row h3{
   cursor:pointer;
 }
+p b{
+		color:#ffffff;
+		display:inline-block;
+		padding:5px 10px;
+		background-color:#c4d7e0;
+		border-radius:2px;
+		text-transform:uppercase;
+		font-size:18px;
+	}
+
 </style>
+
