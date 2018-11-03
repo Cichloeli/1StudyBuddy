907bf595b4c053b657e1a61bf20f78228bd1a6f1
Saiya Youn <sayoun@ucsc.edu>
2018-11-02 18:24:03 -0700

    Added code to register a new user but not functional yet

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,9 +1,35 @@
 <template>
  <div class ="register">
      <p>Let's create a new account!</p>
-     <input type="text" placeholder="Email">
-     <input type="password" placeholder="Password">
-     <button>Register</button>
+     <input type="text" v-model="email" placeholder="Email"><br>
+     <input type="password" v-model="password" placeholder="Password"><br>
+     <button v-on:click="register">Register</button>
      <span>or go back to <router-link to="/login">login</router-link>.</span>
  </div>    
 </template>
+
+<script>
+    import firebase from 'firebase'
+
+    export default {
+        name: 'register',
+        data: function() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            register: function() {
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+                    function (user) {
+                        alert('Your account has been created!')
+                    },
+                    function (err) {
+                        alert('Oops. ' * err.message)
+                    }
+                );
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDWF4Pnu3mBt56EqNwJhHhynhTDQJSMudI",
+  authDomain: "studybuddy-4b778.firebaseapp.com",
+  databaseURL: "https://studybuddy-4b778.firebaseio.com",
+  projectId: "studybuddy-4b778",
+  storageBucket: "studybuddy-4b778.appspot.com",
+  messagingSenderId: "627613848052"
+};
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
