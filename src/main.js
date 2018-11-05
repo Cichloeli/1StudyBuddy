350af9feab2828e3e2c3c2af4fb216eb5f5b9698
EricDunbar <edunbar@ucsc.edu>
2018-11-05 15:13:09 -0800

    refactored code

--- src/main.js
+++ src/main.js
@@ -9,16 +9,7 @@ Vue.config.productionTip = false
 
 let app;
 // firebase init goes here
-let config = {
-  apiKey: "AIzaSyAtTNO9fCUwweAnOXWbbAaOR39t7X1hdCQ",
-  authDomain: "studybuddy-memo.firebaseapp.com",
-  databaseURL: "https://studybuddy-memo.firebaseio.com",
-  projectId: "studybuddy-memo",
-  storageBucket: "studybuddy-memo.appspot.com",
-  messagingSenderId: "986425946679"
-}
 
-firebase.initializeApp(config);
 firebase.auth().onAuthStateChanged(function(user) {
   if (!app) {
     app = new Vue({
