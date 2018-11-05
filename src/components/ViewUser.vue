616405e816701b3caee045839d49ca9bc0f256ab
EricDunbar <edunbar@ucsc.edu>
2018-11-05 15:01:52 -0800

    fixed error message

--- src/components/ViewUser.vue
+++ src/components/ViewUser.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="view-users">
-        <ul class="col"
-        <li class="collection-header"><h4>Users</h4></li>
-        <li v-for="user in users" v-bind:key="user.user_id" 
-        class="collection-item">
-            {{user.name}}:{{user.major}}
-        </li>
+        <ul class="col">
+            <li class="collection-header">
+                <h4>Users</h4>
+            </li>
+            <li v-for="user in users" v-bind:key="user.user_id" 
+                class="collection-item">
+                {{user.name}}:{{user.major}}
+            </li>
         </ul>
     </div>    
 </template>
