5695818c3fcf36acd7ee8383ff22aac2ce9cbfd6
Liren <lwu35@ucsc.edu>
2018-11-20 17:43:54 -0800

    updated profile with schedule

--- src/components/editProfile.vue
+++ src/components/editProfile.vue
@@ -6,7 +6,7 @@
         <div class="title">
             <h1>Edit Your Profile</h1>
         </div>
-        <ul class="col">
+       <!--  <ul class="col">
             <li class="collection-header"><h4>Users</h4></li>
             <li v-for="user in users" v-bind:key="user.user_id" 
             class="collection-item">
@@ -15,16 +15,13 @@
             
          
         </li>
-        </ul>
+        </ul> -->
         
         <div class="info">
             <h6>Name:</h6>
             <input type="checkbox" id="checkName" v-model="checkedName" >
             <div class="infoName">
-                {{ name }}
-                <input type="text" name="nameIn" v-model="input.nameIn" placeholder="Name" class="form-control" style="border-radius:10px;"/>
-                
-                
+                <input type="text" name="nameIn" v-model="input.nameIn" placeholder="Name" class="form-control" style="border-radius:10px;"/>   
             </div>
 
             
@@ -32,45 +29,78 @@
             <h6>Classes:</h6>
             <input type="checkbox" id="checkClasses" v-model="checkedClasses">
             <div class="infoClasses">
-                {{ classes }}
                 <input type="text" name="classesIn" v-model="input.classesIn" placeholder="Classes" class="form-control" style="border-radius:10px;"/>
-                
             </div>
                 
             <h6>Major:</h6>   
              <input type="checkbox" id="checkMajor" v-model="checkedMajor">
             <div class="infoMajor">
-                {{ major }}
-                <input type="text" name="majorIn" v-model="input.majorIn" placeholder="Major" class="form-control" style="border-radius:10px;"/>
-               
+                <input type="text" name="majorIn" v-model="input.majorIn" placeholder="Major" class="form-control" style="border-radius:10px;"/> 
             </div>           
                       
             <h6>Email:</h6>
             <input type="checkbox" id="checkEmail" v-model="checkedEmail">
             <div class="infoEmail">
-                {{ email }}
                 <input type="text" name="emailIn" v-model="input.emailIn" placeholder="Email" class="form-control" style="border-radius:10px;"/>
-                
             </div>
                      
             <h6>About:</h6>
             <input type="checkbox" id="checkAbout" v-model="checkedAbout">
             <div class="infoAbout">
-                {{ about }}
                 <input type="text" name="aboutIn" v-model="input.aboutIn" placeholder="About" class="form-control" style="border-radius:10px;" />
-                
             </div>               
                     
-    
-        
+        </div>
+
+        <div class="updateButton">
+            <button class="btn btn-lg btn-primary btn-block" type="button" v-on:click="update()">Update Profile</button>
+        </div>
+
+        <div class="title">
+            <h1>Schedule</h1>
+        </div>
+
+        <div class="schedule">
+
+            
+            <div class="sunday">
+                <h6>Sunday:</h6>
+                <input type="text" name="sundayIn" v-model="input.sundayIn" placeholder="time" class="form-control" style="border-radius:10px;" />
+            </div> 
+            <div class="monday">
+                <h6>Monday:</h6>
+                <input type="text" name="mondayIn" v-model="input.mondayIn" placeholder="time" class="form-control" style="border-radius:10px;" />
+            </div> 
+            
+            <div class="tuesday">
+                <h6>Tuesday:</h6>
+                <input type="text" name="tuesdayIn" v-model="input.tuesdayIn" placeholder="time" class="form-control" style="border-radius:10px;" />
+            </div> 
+            <div class="wednesday">
+                <h6>Wednesday:</h6>
+                <input type="text" name="wednesdayIn" v-model="input.wednesdayIn" placeholder="time" class="form-control" style="border-radius:10px;" />
+            </div> 
+            <div class="thursday">
+                 <h6>Thursday:</h6>
+                <input type="text" name="thursdayIn" v-model="input.thursdayIn" placeholder="time" class="form-control" style="border-radius:10px;" />
+            </div> 
+            <div class="friday">
+                <h6>Friday:</h6>
+                <input type="text" name="fridayIn" v-model="input.fridayIn" placeholder="time" class="form-control" style="border-radius:10px;" />
+            </div> 
+            <div class="saturday">
+                <h6>Saturday:</h6>
+                <input type="text" name="saturdayIn" v-model="input.saturdayIn" placeholder="time" class="form-control" style="border-radius:10px;" />
+            </div>    
+
         </div>
         
-        <div class="updateButton">
-            <button class="btn btn-lg btn-primary btn-block" type="button" v-on:click="update()">Update</button>
+        <div class="updateScheduleButton">
+            <button class="btn btn-lg btn-primary btn-block" type="button" v-on:click="updateSchedule()">Update Schedule</button>
         </div>
-        <div class="updateButton">
+        <!-- <div class="updateButton">
             <button class="btn btn-lg btn-primary btn-block" type="button" v-on:click="info()">Info</button>
-        </div>
+        </div> -->
         
     </div>
     
@@ -95,6 +125,14 @@ export default {
         aboutIn: null,
         emailIn: null,
 
+        sundayIn: null,
+        mondayIn: null,
+        tuesdayIn: null,
+        wednesdayIn: null,
+        thursdayIn: null,
+        fridayIn: null,
+        saturdayIn: null,
+
         
       },
       checkedName: true,
@@ -111,6 +149,14 @@ export default {
       major: null,
       about: null,
       email: null,
+
+      sunday: null,
+      monday: null,
+      tuesday: null,
+      wednesday: null,
+      thursday: null,
+      friday: null,
+      saturday: null,
       header: require("../assets/images/p2.jpg"),
     };
   },
@@ -162,8 +208,57 @@ export default {
             this.$router.replace('profile')
         
         },
+        updateSchedule() {
+            console.log("update schedule");
+
+            if (this.input.sundayIn!=null ) {
+                firebase.firestore().collection('users').doc(currentUser.uid).update({
+                sunday: this.input.sundayIn,
+                })
+            }
+
+            if (this.input.mondayIn!=null) {
+                firebase.firestore().collection('users').doc(currentUser.uid).update({
+                monday: this.input.mondayIn,
+               
+                })
+            }
+
+            if (this.input.tuesdayIn!=null) {
+                firebase.firestore().collection('users').doc(currentUser.uid).update({
+                tuesday: this.input.tuesdayIn,
+                })
+            }
+
+            if (this.input.wednesdayIn!=null) {
+                firebase.firestore().collection('users').doc(currentUser.uid).update({
+                wednesday: this.input.wednesdayIn,
+                })
+            }
+
+            if (this.input.thursdayIn!=null) {
+                firebase.firestore().collection('users').doc(currentUser.uid).update({
+                thursday: this.input.thursdayIn,
+                })
+            }
+
+            if (this.input.fridayIn!=null) {
+                firebase.firestore().collection('users').doc(currentUser.uid).update({
+                friday: this.input.fridayIn,
+                })
+            }
+
+            if (this.input.saturdayIn!=null) {
+                firebase.firestore().collection('users').doc(currentUser.uid).update({
+                saturday: this.input.saturdayIn,
+                })
+            }
+            
+            
+            this.$router.replace('profile')
+        
+        },
         info(){
-                test();
                 console.log("info");
                 firebase.firestore().collection("users").doc(currentUser.uid)
                 .get().then(function(doc) {
@@ -225,6 +320,7 @@ export default {
 }
 .title {
   text-align: left;
+  padding: 20px;
 }
 
 .info {
@@ -265,7 +361,60 @@ export default {
 }
 .updateButton{
     max-width: 150px;
+    margin-left: 85%;
     
+}
+.schedule {
+    padding: 10px;
+    max-width: 1000px;
+
+}
+
+.schedule h6 {
+    font-weight: bold;
+    padding: 10px;
+}
+
+.schedule input {
+    padding: 10px;
+}
+
+.updateScheduleButton{
+    max-width: 200px;
+    margin-left: 80%;
+}
+
+.sunday {
+    display: inline-block;
+    padding-inline-end: 20px;
+
+}
+
+.monday {
+    display: inline-block;
+    padding-inline-end: 20px;
+}
+
+.tuesday{
+    display: inline-block;
+    padding-inline-end: 20px;
+}
+.wednesday{
+    display: inline-block;
+    padding-inline-end: 20px;
+}
+
+.thursday {
+    display: inline-block;
+    padding-inline-end: 20px;
+}
+.friday {
+    display: inline-block;
+    padding-inline-end: 20px;
+}
+.saturday {
+    display:inline-block;
+    padding-inline-end: 20px;
 }
 
 
