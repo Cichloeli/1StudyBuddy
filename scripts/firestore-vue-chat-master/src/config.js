2bab177a424ad7a0ff06385a60e478ce7059c8b7
yli331 <yli331@ucsc.edu>
2018-11-25 20:19:57 -0800

    chat

---
+++ scripts/firestore-vue-chat-master/src/config.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyAtTNO9fCUwweAnOXWbbAaOR39t7X1hdCQ",
+    authDomain: "studybuddy-memo.firebaseapp.com",
+    databaseURL: "https://studybuddy-memo.firebaseio.com",
+    projectId: "studybuddy-memo",
+    storageBucket: "studybuddy-memo.appspot.com",
+    messagingSenderId: "986425946679"
+};
+export default config
