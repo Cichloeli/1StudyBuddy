80c5a5a26bd6510ecc83173039a6b1861d99bfc5
Saiya Youn <sayoun@ucsc.edu>
2018-11-27 14:55:04 -0800

    added upload button and submit button

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,14 +1,32 @@
 <template>
-<div id= container>
-    <h1>Upload</h1>
-    <div class = body>
-
-    </div>
+<div id="container">
+    <h1>Upload File</h1>
+    <label class="upload-group">
+        <!-- let's you select a file from your computer -->
+        <input type="file" class="upload-group" id="file">
+    </label>
+    <button type="btn btn-primary" id="uploadButton" onclick="uploadFile()">Submit</button>
 </div>    
 </template>
 
+<script>
+</script>
+
 <style>
+#container{
+    margin-top: 40px;
+}
 h1{
-    font-size: 15px;
+    font-size: 30px;
+    text-align: left;
+}
+label.upload-group{
+    width: 60px;
+    height: 20px;
+    margin-right: 1050px;
+}
+#uploadButton{
+    margin-right: 1050px;
+    margin-top: 15px;
 }
 </style>
