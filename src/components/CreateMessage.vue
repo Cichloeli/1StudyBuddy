569e9b9af83412dcd4043efe4b3ed2e263c7689d
Cecilia Li <39076312+Cichloeli@users.noreply.github.com>
2018-11-30 00:40:26 -0800

    Update CreateMessage.vue

--- src/components/CreateMessage.vue
+++ src/components/CreateMessage.vue
@@ -24,6 +24,7 @@
                 errorText: null
             }
         },
+        //takes in input, adds name of the user, the time into database
         methods: {
             createMessage () {
                 if (this.newMessage) {
