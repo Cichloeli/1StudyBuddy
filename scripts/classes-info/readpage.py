c43d1d969fe19664df7b4ef75f9ed39c2b899ad8
Cecilia Li <39076312+Cichloeli@users.noreply.github.com>
2018-11-30 18:45:03 -0800

    Update readpage.py

--- scripts/classes-info/readpage.py
+++ scripts/classes-info/readpage.py
@@ -3,6 +3,7 @@ import urllib
 outputFile=open('classes1.txt','w')
 
 #read page => updateclasses => createjson
+#Readpage he course information from the school's website then toutputs the html as a txt file
 
 link = "https://courses.soe.ucsc.edu/courses/cmps"
 f = urllib.urlopen(link)
